54bdb7da241ec8864dd1414cabe97caac5005fac
hollylxj <lianghollyxuejiao@gmail.com>
2018-05-27 10:54:33 +0000

    visualization

--- PyTorch-SRGAN/train.py
+++ PyTorch-SRGAN/train.py
@@ -24,17 +24,17 @@ from PIL import Image
 from scipy.misc import imsave
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--dataset', type=str, default='cifar100', help='cifar10 | cifar100 | folder')
+parser.add_argument('--dataset', type=str, default='anime-faces', help='cifar10 | cifar100 | folder')
 parser.add_argument('--dataroot', type=str, default='../anime-faces', help='path to dataset')
-parser.add_argument('--workers', type=int, default=2, help='number of data loading workers')
+parser.add_argument('--workers', type=int, default=12, help='number of data loading workers')
 parser.add_argument('--batchSize', type=int, default=16, help='input batch size')
 parser.add_argument('--imageSize', type=int, default=15, help='the low resolution image size')
 parser.add_argument('--upSampling', type=int, default=2, help='low to high resolution scaling factor')
 parser.add_argument('--nEpochs', type=int, default=100, help='number of epochs to train for')
 parser.add_argument('--generatorLR', type=float, default=0.0001, help='learning rate for generator')
 parser.add_argument('--discriminatorLR', type=float, default=0.0001, help='learning rate for discriminator')
-parser.add_argument('--cuda', action='store_true', help='enables cuda')
-parser.add_argument('--nGPU', type=int, default=1, help='number of GPUs to use')
+parser.add_argument('--cuda', default=True, help='enables cuda')
+parser.add_argument('--nGPU', type=int, default=2, help='number of GPUs to use')
 parser.add_argument('--generatorWeights', type=str, default='', help="path to generator weights (to continue training)")
 parser.add_argument('--discriminatorWeights', type=str, default='', help="path to discriminator weights (to continue training)")
 parser.add_argument('--out', type=str, default='checkpoints', help='folder to output model checkpoints')
@@ -47,8 +47,6 @@ try:
 except OSError:
     pass
 
-if torch.cuda.is_available() and not opt.cuda:
-    print("WARNING: You have a CUDA device, so you should probably run with --cuda")
 
 transform = transforms.Compose([transforms.RandomCrop(opt.imageSize*opt.upSampling),
                                 transforms.ToTensor()])
@@ -69,16 +67,19 @@ dataset = datasets.ImageFolder(root=opt.dataroot, transform=transform)
 dataloader = torch.utils.data.DataLoader(dataset, batch_size=opt.batchSize,
                                          shuffle=True, num_workers=int(opt.workers))
 
+##########################################################################################
+# load weights
+##########################################################################################
 generator = Generator(16, opt.upSampling)
-if opt.generatorWeights != '':
-    generator.load_state_dict(torch.load(opt.generatorWeights))
-print(generator)
-
 discriminator = Discriminator()
-if opt.discriminatorWeights != '':
-    discriminator.load_state_dict(torch.load(opt.discriminatorWeights))
-print(discriminator)
+# if opt.generatorWeights != '':
+#     generator.load_state_dict(torch.load(opt.generatorWeights))
+# print(generator)
+# if opt.discriminatorWeights != '':
+#     discriminator.load_state_dict(torch.load(opt.discriminatorWeights))
+# print(discriminator)
 
+##########################################################################################
 # For the content loss
 feature_extractor = FeatureExtractor(torchvision.models.vgg19(pretrained=True))
 print(feature_extractor)
@@ -88,6 +89,7 @@ adversarial_criterion = nn.BCELoss()
 ones_const = Variable(torch.ones(opt.batchSize, 1))
 
 # if gpu is to be used
+print("gpu:", opt.cuda)
 if opt.cuda:
     generator.cuda()
     discriminator.cuda()
@@ -99,72 +101,59 @@ if opt.cuda:
 optim_generator = optim.Adam(generator.parameters(), lr=opt.generatorLR)
 optim_discriminator = optim.Adam(discriminator.parameters(), lr=opt.discriminatorLR)
 
-configure('logs/' + opt.dataset + '-' + str(opt.batchSize) + '-' + str(opt.generatorLR) + '-' + str(opt.discriminatorLR), flush_secs=5)
+# configure('logs/' + opt.dataset + '-' + str(opt.batchSize) + '-' + str(opt.generatorLR) + '-' + str(opt.discriminatorLR), flush_secs=5)
 #visualizer = Visualizer(image_size=opt.imageSize*opt.upSampling)##############################
 
 low_res = torch.FloatTensor(opt.batchSize, 3, opt.imageSize, opt.imageSize)
 
 # Pre-train generator using raw MSE loss
 print('Generator pre-training')
-for epoch in range(2):
-    mean_generator_content_loss = 0.0
+# for epoch in range(2):
+#     mean_generator_content_loss = 0.0
 
-    for i, data in enumerate(dataloader):
-        # Generate data
-        high_res_real, _ = data
+#     for i, data in enumerate(dataloader):
+#         # Generate data
+#         high_res_real, _ = data
 
-        # Downsample images to low resolution
-        for j in range(opt.batchSize):
-            low_res[j] = scale(high_res_real[j])
-            high_res_real[j] = normalize(high_res_real[j])
+#         # Downsample images to low resolution
+#         for j in range(opt.batchSize):
+#             low_res[j] = scale(high_res_real[j])
+#             high_res_real[j] = normalize(high_res_real[j])
 
-        # Generate real and fake inputs
-        if opt.cuda:
-            high_res_real = Variable(high_res_real.cuda())
-            high_res_fake = generator(Variable(low_res).cuda())
-        else:
-            high_res_real = Variable(high_res_real)
-            high_res_fake = generator(Variable(low_res))
+#         # Generate real and fake inputs
+#         if opt.cuda:
+#             high_res_real = Variable(high_res_real.cuda())
+#             high_res_fake = generator(Variable(low_res).cuda())
+#         else:
+#             high_res_real = Variable(high_res_real)
+#             high_res_fake = generator(Variable(low_res))
 
-        ######### Train generator #########
-        generator.zero_grad()
+#         ######### Train generator #########
+#         generator.zero_grad()
 
-        generator_content_loss = content_criterion(high_res_fake, high_res_real)
-        mean_generator_content_loss += generator_content_loss.data[0]
+#         generator_content_loss = content_criterion(high_res_fake, high_res_real)
+#         mean_generator_content_loss += generator_content_loss.data[0]
 
-        generator_content_loss.backward()
-        optim_generator.step()
+#         generator_content_loss.backward()
+#         optim_generator.step()
 
-        ######### Status and display #########
-        sys.stdout.write('\r[%d/%d][%d/%d] Generator_MSE_Loss: %.4f' % (epoch, 2, i, len(dataloader), generator_content_loss.data[0]))
-        #visualizer.show(low_res, high_res_real.cpu().data, high_res_fake.cpu().data)#######################
-        if i % 100 == 0:
-            # the first 64 samples from the mini-batch are saved.
-#             print(np.shape(high_res_fake.cpu().data))
-#             for ind in range(16):
-#                 image = high_res_fake.cpu().data[ind]
-#                 image.sub_(0.5).div_(0.5) 
-#             vutils.save_image(low_res.data[0:16,:,:,:],
-#                     '%s/low_res.png' % opt.out, nrow = 4)
-               
-#                 A = np.array(image).reshape(30,30,3)
-#                 print(np.shape(A))
-#                 print(A)
-#                 imsave('%s/fake_samples_epoch_%03d_%03d.png' % (opt.out, epoch, ind), A)
-            vutils.save_image(high_res_fake.cpu().data,
-                    '%s/fake_samples_epoch_%03d.jpg' % (opt.out, epoch))
-            vutils.save_image(low_res.cpu().data,
-                    '%s/low_res_samples_epoch_%03d.jpg' % (opt.out, epoch))
-#             torch.save(generator.state_dict(), '%s/generator_pretrain.pth' % opt.out)
+#         ######### Status and display #########
+#         sys.stdout.write('\r[%d/%d][%d/%d] Generator_MSE_Loss: %.4f' % (epoch, 2, i, len(dataloader), generator_content_loss.data[0]))
+#         if i % 100 == 0:
+
+#             vutils.save_image(high_res_fake.cpu().data,
+#                     '%s/fake_samples_epoch_%03d.jpg' % (opt.out, epoch))
+#             vutils.save_image(low_res.cpu().data,
+#                     '%s/low_res_samples_epoch_%03d.jpg' % (opt.out, epoch))
+
         
         
-
-#     sys.stdout.write('\r[%d/%d][%d/%d] Generator_MSE_Loss: %.4f\n' % (epoch, 2, i, len(dataloader), mean_generator_content_loss/len(dataloader)))
-#     log_value('generator_mse_loss', mean_generator_content_loss/len(dataloader), epoch)
 
 # Do checkpointing
-torch.save(generator.state_dict(), '%s/generator_pretrain.pth' % opt.out)
+# torch.save(generator.state_dict(), '%s/generator_pretrain.pth' % opt.out)
 
+
+generator.load_state_dict(torch.load('checkpoints/generator_pretrain.pth'))
 # SRGAN training
 optim_generator = optim.Adam(generator.parameters(), lr=opt.generatorLR*0.1)
 optim_discriminator = optim.Adam(discriminator.parameters(), lr=opt.discriminatorLR*0.1)
@@ -179,6 +168,11 @@ for epoch in range(opt.nEpochs):
     for i, data in enumerate(dataloader):
         # Generate data
         high_res_real, _ = data
+        
+        if len(high_res_real) < opt.batchSize:
+            print("high_res_real data not enough")
+            print("data: ", len(high_res_real), "batch_size: ", opt.batchSize)
+            break;
 
         # Downsample images to low resolution
         for j in range(opt.batchSize):
@@ -227,22 +221,14 @@ for epoch in range(opt.nEpochs):
         ######### Status and display #########
         sys.stdout.write('\r[%d/%d][%d/%d] Discriminator_Loss: %.4f Generator_Loss (Content/Advers/Total): %.4f/%.4f/%.4f' % (epoch, opt.nEpochs, i, len(dataloader),
         discriminator_loss.data[0], generator_content_loss.data[0], generator_adversarial_loss.data[0], generator_total_loss.data[0]))
-        #visualizer.show(low_res, high_res_real.cpu().data, high_res_fake.cpu().data)#######################
+
+        ######### update images saved #########
         if i % 100 == 0:
             # the first 64 samples from the mini-batch are saved.
             vutils.save_image(high_res_fake.cpu().data,
                     '%s/fake_samples_epoch_%03d.jpg' % (opt.out, epoch))
             vutils.save_image(low_res.cpu().data,
                     '%s/low_res_samples_epoch_%03d.jpg' % (opt.out, epoch))
-        
-#     sys.stdout.write('\r[%d/%d][%d/%d] Discriminator_Loss: %.4f Generator_Loss (Content/Advers/Total): %.4f/%.4f/%.4f\n' % (epoch, opt.nEpochs, i, len(dataloader),
-#     mean_discriminator_loss/len(dataloader), mean_generator_content_loss/len(dataloader), 
-#     mean_generator_adversarial_loss/len(dataloader), mean_generator_total_loss/len(dataloader)))
-
-#     log_value('generator_content_loss', mean_generator_content_loss/len(dataloader), epoch)
-#     log_value('generator_adversarial_loss', mean_generator_adversarial_loss/len(dataloader), epoch)
-#     log_value('generator_total_loss', mean_generator_total_loss/len(dataloader), epoch)
-#     log_value('discriminator_loss', mean_discriminator_loss/len(dataloader), epoch)
 
     # Do checkpointing
     torch.save(generator.state_dict(), '%s/generator_final.pth' % opt.out)
